665d54aaf196d7850077b05e048b2ed744964aa4
0xff <worktrabalho633@gmail.com>
2025-07-30 21:21:37 +0100

    add: lita de vidoes

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -5,10 +5,11 @@ export default function Videos(){
   const { data, isPending } = useGetAllVideos();
   
   return(
-    <div>
-      <h2 className="uppercase">Vidoes em destaques</h2> 
-      <div className="flex flex-col items-center">
-        <ul className="grid sm:grid-cols-2 md:grid-cols-3">
+    <div className="py-3">
+      <h2 className="uppercase text-2xl font-bold">Vidoes em destaques</h2> 
+      <div className="flex flex-col items-center my-3">
+        {isPending && <p className="font-medium animate-pulse">Carregando Videos ...</p>}
+        <ul className="grid gap-3 sm:grid-cols-2 md:grid-cols-3">
           {data?.map(props =>(
             <li><Video {...props}/></li>
           ))}
