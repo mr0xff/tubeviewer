6e3a932dd556bd102bf1ad07affe5f963db75015
0xff <worktrabalho633@gmail.com>
2025-07-31 23:45:21 +0100

    add: estratégia de cache no cliente

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import type { Video } from '@/lib/types';
+import { BrowserCache } from './utils';
 
 const instance = axios.create({ baseURL: "https://www.googleapis.com/youtube/v3/" });
+
+const cache = new BrowserCache();
 
 export async function getVidoes(count: number): Promise<Video[]>{
   const params = {
@@ -9,17 +12,25 @@ export async function getVidoes(count: number): Promise<Video[]>{
     channelId: import.meta.env.VITE_CHANNEL_ID,
     part: 'snippet,id',
     order: 'date',
-    maxResults: count,
+    maxResults: 100,
     type: 'video' // só vídeos, ignora playlists ou canais
   };
 
-  const res = await instance.get("/search", { params });
+  const res = !cache.hasCache() 
+    ? (await instance.get("/search", { params })).data
+    : cache.getVideos()
+  
+  if(!cache.hasCache()){
+    const formatedVideos = res.items.map((item: any) => ({
+      title: item.snippet.title,
+      videoId: item.id.videoId,
+      thumbnail: item.snippet.thumbnails.medium.url,
+      publishedAt: item.snippet.publishedAt,
+      videoUrl: `https://www.youtube.com/watch?v=${item.id.videoId}`
+    }));
+    
+    cache.setCache(formatedVideos);
+  }
 
-  return res.data.items.map((item: any) => ({
-    title: item.snippet.title,
-    videoId: item.id.videoId,
-    thumbnail: item.snippet.thumbnails.medium.url,
-    publishedAt: item.snippet.publishedAt,
-    videoUrl: `https://www.youtube.com/watch?v=${item.id.videoId}`
-  }));
+  return cache.getVideos().slice(0, count >= 100 ? 99:count);
 }
---
+++ src/lib/utils.ts
@@ -0,0 +1,37 @@
+import type { Video } from "./types";
+
+export class BrowserCache {
+  #storageKey = "tubeviwer";
+  #cacheInMemory: Map<string, Video> = new Map(); // pode ser passado para Set's
+
+  setCache(data: unknown){
+    if(data){
+      localStorage.setItem(this.#storageKey, JSON.stringify(data));
+      this.#loadDataToMemory();
+    }
+  }
+
+  #loadDataToMemory(){
+    const data = localStorage.getItem(this.#storageKey);
+    if(data){
+      const parsed = JSON.parse(data) as Video[];
+      parsed.forEach(data => {
+        this.#cacheInMemory.set(data.videoId, data);
+      });
+    }
+  }
+
+  sync(){
+    localStorage.clear();
+  }
+
+  getVideos(){
+    this.#loadDataToMemory();
+    console.log(Array.from(this.#cacheInMemory.values()))
+    return Array.from(this.#cacheInMemory.values());
+  }
+
+  hasCache(){
+    return this.getVideos().length > 0;
+  }
+}
