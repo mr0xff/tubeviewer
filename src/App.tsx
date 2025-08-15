29a74810b58ef878a7608a88b19d847c886e1a7b
キリスト・ムアコ <112662247+mr0xff@users.noreply.github.com>
2025-08-15 17:14:20 +0100

    Revert "Master"

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import RootLayout from "@/components/RootLayout";
 import Home from "@/pages/Home";
 import Videos from "./pages/Videos";
 import VideoWatch from "./pages/VideoWatch";
-import Producoes from "./pages/Producoes";
-import Sobre from "./pages/Sobre";
-import Contato from "./pages/Contato";
 
 export default function App(){
   return(
@@ -14,9 +11,6 @@ export default function App(){
         <Route index element={<Home />} />
         <Route path="videos" element={<Videos />} />
         <Route path="watch/:videoId" element={<VideoWatch />} />
-        <Route path="producoes" element={<Producoes />} />
-        <Route path="sobre" element={<Sobre />} />
-        <Route path="contato" element={<Contato />} />
       </Route>
     </Routes>
   )
